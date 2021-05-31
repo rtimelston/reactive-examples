91cddc46a370b8c72efbb79a48bf1f68c78f18cb
rtimelston <13246079+rtimelston@users.noreply.github.com>
2021-05-31 10:46:22 -0700

    Section 2.14 Mono Operations

--- src/test/java/guru/springframework/reactiveexamples/domain/PersonRepositoryImplTest.java
+++ src/test/java/guru/springframework/reactiveexamples/domain/PersonRepositoryImplTest.java
@@ -1,4 +1,5 @@
 package guru.springframework.reactiveexamples.domain;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
@@ -8,20 +9,46 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class PersonRepositoryImplTest {
 
-    @Test
-    @DisplayName("getById gets existing person by id")
-    void test_getById_returns_existing_person() {
-        PersonRepository repo = new PersonRepositoryImpl();
-        Person expected = new Person(1, "Michael", "Weston");
-        Mono<Person> actual = repo.getById(1);
-        assertEquals(expected, actual.block());
+    PersonRepository personRepository;
+    Person expectedPerson;
+
+    @BeforeEach
+    void setup() {
+        personRepository = new PersonRepositoryImpl();
+        expectedPerson = new Person(1, "Michael", "Weston");
     }
 
     @Test
-    @DisplayName("getById returns null for non-existing person")
-    void testGetById() {
-        PersonRepository repo = new PersonRepositoryImpl();
-        Mono<Person> actual = repo.getById(-1);
-        assertNull(actual);
+    @DisplayName("getById - block gets existing person by id")
+    void getById_BlockReturnsExistingPerson() {
+        Mono<Person> actualPerson = personRepository.getById(1);
+        assertEquals(expectedPerson, actualPerson.block());
+    }
+
+    @Test
+    @DisplayName("getById - Subscribe gets existing person by id")
+    void getById_SubscribeReturnsExistingPerson() {
+        Mono<Person> actualPerson = personRepository.getById(1);
+        actualPerson.subscribe(person -> {
+            assertEquals(expectedPerson, actualPerson.block());
+        });
+    }
+
+    @Test
+    @DisplayName("getById - Subscribe gets existing person by id")
+    void getById_SubscribeReturnsFirstNameFromMap() {
+        Mono<Person> actualPerson = personRepository.getById(1);
+        actualPerson
+                .map(person -> person.getFirstName())
+                .subscribe(firstName -> {
+                    assertEquals(expectedPerson.getFirstName(), firstName);
+                });
+    }
+
+    @Test
+    @DisplayName("getById - returns null for non-existing person")
+    void getById_ReturnsNullForNonExistingPerson() {
+        Mono<Person> actualPerson = personRepository.getById(-1);
+        assertNull(actualPerson);
     }
 }
