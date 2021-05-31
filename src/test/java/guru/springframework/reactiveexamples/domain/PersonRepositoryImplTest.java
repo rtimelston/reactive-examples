274f550a3a2ef4e98f07102324b7e98c6f623235
rtimelston <13246079+rtimelston@users.noreply.github.com>
2021-05-31 11:20:08 -0700

    Section 2.15 Flux Operations

--- src/test/java/guru/springframework/reactiveexamples/domain/PersonRepositoryImplTest.java
+++ src/test/java/guru/springframework/reactiveexamples/domain/PersonRepositoryImplTest.java
@@ -2,7 +2,12 @@ package guru.springframework.reactiveexamples.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -50,5 +55,44 @@ public class PersonRepositoryImplTest {
     void getById_ReturnsNullForNonExistingPerson() {
         Mono<Person> actualPerson = personRepository.getById(-1);
         assertNull(actualPerson);
+    }
+
+    @Test
+    @DisplayName("findAll - blockFirst returns first person")
+    void findAll_BlockFirstReturnsFirstPerson() {
+        Flux<Person> personFlux = personRepository.findAll();
+        Person personActual = personFlux.blockFirst();
+        assertEquals(expectedPerson, personActual);
+    }
+
+    @Test
+    @DisplayName("findAll - subscribe returns all persons")
+    void findAll_SubscribeReturnsAllPersons() {
+        Map<Integer, Person> personIds = new HashMap<>();
+        Flux<Person> personFlux = personRepository.findAll();
+        personFlux.subscribe(person -> {
+            personIds.put(person.getId(), person);
+        });
+        assertEquals(4, personIds.size());
+        assert(personIds.containsKey(1));
+        assert(personIds.containsKey(2));
+        assert(personIds.containsKey(3));
+        assert(personIds.containsKey(4));
+        assert(!personIds.containsKey(5));
+    }
+
+    @Test
+    @DisplayName("findAll - collectList returns all persons")
+    void findAll_CollectListReturnsAllPersons() {
+        Flux<Person> personFlux = personRepository.findAll();
+        Mono<List<Person>> personListMono = personFlux.collectList();
+        personListMono.subscribe(list -> {
+            assertEquals(4, list.size());
+            assert(list.contains(new Person(1, "", "")));
+            assert(list.contains(new Person(2, "", "")));
+            assert(list.contains(new Person(3, "", "")));
+            assert(list.contains(new Person(4, "", "")));
+            assert(!list.contains(new Person(5, "", "")));
+        });
     }
 }
